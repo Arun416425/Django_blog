bc24439f43f05533c056589c95d51d21b8b94e00
Arun Ayyori <arunayyori123@gmail.com>
2026-03-14 12:40:46 +0530

    added comment feature

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Blog
+from .models import Category, Blog, Comment
 
 
 class BlogAdmin(admin.ModelAdmin):
@@ -15,4 +15,5 @@ class BlogAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Category)
+admin.site.register(Comment)
 admin.site.register(Blog, BlogAdmin)
