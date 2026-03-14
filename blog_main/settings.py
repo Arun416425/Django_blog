58c7fbc6ac81a0e978f2e3240924bca996ec2c08
Arun Ayyori <arunayyori123@gmail.com>
2026-03-14 15:10:59 +0530

    Modify STATIC_URL and STATICFILES_DIRS settings
    Updated STATICFILES_DIRS to include blog_main/static.

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "blog_main/static",
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
